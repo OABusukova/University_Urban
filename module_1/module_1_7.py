d42890eecdd05938cf401207d57eb6924d6b688b
OABusukova <obusukova@gmail.com>
2024-11-13 22:02:28 +0300

    Переделала модуль 1_7

--- module_1/module_1_7.py
+++ module_1/module_1_7.py
@@ -1,16 +1,10 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]  # значение
 students = {'Johny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}  # ключи
 dict = {}  # пустой словарь
-dict.update({'Aaron': [5, 3, 3, 5, 4], 'Bilbo': [2, 2, 2, 3], 'Johny': [4, 5, 5, 2], 'Khendrik': [4, 4, 3],
-             'Steve': [5, 5, 5, 4, 5]})  # добавила значения в пустой словарь
-Aaron = [5, 3, 3, 5, 4]
-Bilbo = [2, 2, 2, 3]
-Johny = [4, 5, 5, 2]
-Khendrik = [4, 4, 3]
-Steve = [5, 5, 5, 4, 5]
-dict['Aaron'] = (sum(Aaron) / len(Aaron))
-dict['Bilbo'] = (sum(Bilbo) / len(Bilbo))
-dict['Johny'] = (sum(Johny) / len(Johny))
-dict['Khendrik'] = (sum(Khendrik) / len(Khendrik))
-dict['Steve'] = (sum(Steve) / len(Steve))
+students_sort = sorted(students)  # сортировка ключей
+dict[students_sort[0]] = sum(grades[0])/len(grades[0])
+dict[students_sort[1]] = sum(grades[1])/len(grades[1])
+dict[students_sort[2]] = sum(grades[2])/len(grades[2])
+dict[students_sort[3]] = sum(grades[3])/len(grades[3])
+dict[students_sort[4]] = sum(grades[4])/len(grades[4])
 print(dict)
