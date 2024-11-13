e4bf1c6777848df2cdd85616473c4ea84928f134
OABusukova <obusukova@gmail.com>
2024-11-13 13:49:55 +0300

    Перенесли из файла main в файл module_1_2 решешние по задаче "переменные"

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-homework = 12
-hours_spent = 1.5
-course_name = 'Python'
-time_for_1_task = 1.5 / 12
-print("Курс: " + course_name + ", всего задач: " + str(homework) + ", затрачено часов: " + str(
-    hours_spent) + ", среднее время выполнения: " + str(time_for_1_task) + ' часа.')
+
 
 example = 'Топинамбур'
 print(example[0])
---
+++ module_1/module_1_2.py
@@ -0,0 +1,6 @@
+homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_1_task = 1.5 / 12
+print("Курс: " + course_name + ", всего задач: " + str(homework) + ", затрачено часов: " + str(
+    hours_spent) + ", среднее время выполнения: " + str(time_for_1_task) + ' часа.')
