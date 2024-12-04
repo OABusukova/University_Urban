7253ffe912ae15bed001eb6bb7a3e28d16464cee
OABusukova <obusukova@gmail.com>
2024-12-04 14:19:03 +0300

    Модуль 3, задание 2 исправила.

--- module_3/module_3_2.py
+++ module_3/module_3_2.py
@@ -2,13 +2,15 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
     if '@' not in recipient or '@' not in sender \
             or not recipient.endswith((".com", ".ru", ".net")) \
             or not sender.endswith((".com", ".ru", ".net")):
-        print("Невозможно отправить письмо с адреса <sender> на адрес <recipient>")
+        print('Невозможно отправить письмо с адреса', sender + ' на адрес', recipient)
+    elif recipient == 'vasyok1337@gmail.com':
+        print('Письмо успешно отправлено с адреса', sender + ' на адрес', recipient)
     elif recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == sender:
-        print("Письмо успешно отправлено с адреса <sender> на адрес <recipient>.")
-    else:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.")
+        print('Нельзя отправить письмо самому себе!')
+    elif sender != 'university.help@gmail.com':
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender + ' на адрес', recipient)
+    elif sender == 'urban.info@gmail.com':
+        print('Письмо успешно отправлено с адреса', sender + ' на адрес', recipient)
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
