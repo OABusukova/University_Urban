3a790a8479c9127a064197dfb903265eca0d3981
OABusukova <obusukova@gmail.com>
2024-12-23 13:34:35 +0300

    Модуль 4 остатки, модуль 5, задание 1.

---
+++ module_4/ins.py
@@ -0,0 +1,9 @@
+from module_4.alg import *
+data_1 = list(map(int, input('Введите числа через пробел: ').split()))
+data_2 = list(map(int, input('Введите числа через пробел: ').split()))
+
+bubble_sort(data_1)
+selection_sort(data_2)
+
+print('Пузырьковая сортировка: ', data_1)
+print('Сортировка выбором: ', data_2)
